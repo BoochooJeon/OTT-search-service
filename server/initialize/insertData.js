52cbfdfe74ac3bac874d44e7dd5912792463aaca
BoochooJeon <dgwogh@gmail.com>
2023-11-22 00:55:33 +0900

    [Fix] minor fix

--- server/initialize/insertData.js
+++ server/initialize/insertData.js
@@ -53,28 +53,28 @@ function insertMovies() {
         ('인디아나 존스: 운명의 다이얼', 'James Mangold', 'Adventure', 154, '/posters/인디아나_존스_운명의_다이얼.jpeg'),
         ('캐리비안의 해적 - 블랙 펄의 저주', 'Gore Verbinski', 'Adventure', 143, '/posters/캐리비안의_해적_블랙펄의_저주.jpeg'),
         ('에브리씽 에브리웨어 올 앳 원스', 'Daniel Kwan', 'Comedy', 139, '/posters/에브리씽_에브리웨어_올_앳_원스.jpeg'),
-        ('극한직업', '이병헌', 'Comedy', 111, '/posters/극한직업.jpeg');
-        ('화사한 그녀', '이승준', 'Comedy', 121, '/posters/화사한_그녀.jpeg');
-        ('30일', '남대중', 'Comedy', 119, '/posters/30일.jpeg');
-        ('컨저링2', 'James Wan', 'Horror', 134, '/posters/컨저링2.jpeg');
-        ('곡성', '나홍진', 'Horror', 156, '/posters/곡성.jpeg');
-        ('엑스텐션', 'Alexandre Aja', 'Horror', 85, '/posters/엑스텐션.jpeg');
-        ('라이트 아웃', 'David F. Sandberg', 'Horror', 81, '/posters/라이트_아웃.jpeg');
-        ('미녀와 야수', 'Bill Condon', 'Romance', 129, '/posters/미녀와_야수.jpeg');
-        ('노트북', 'Nick Cassavetes', 'Romance', 123, '/posters/노트북.jpeg');
-        ('어바웃 타임', 'Richard Curtis', 'Romance', 123, '/posters/어바웃_타임.jpeg');
-        ('500일의 썸머', 'Marc Webb', 'Romance', 95, '/posters/500일의_썸머.jpeg');
-        ('크리에이터', 'Gareth Edwards', 'SF', 133, '/posters/크리에이터.jpeg');
-        ('인터스텔라', 'Christopher Nolan', 'SF', 169, '/posters/인터스텔라.jpeg');
-        ('레디 플레이어 원', 'Steven Spielberg', 'SF', 140, '/posters/레디_플레이어_원.jpeg');
-        ('듄', 'Denis Villeneuve', 'SF', 155, '/posters/듄.jpeg');
-        ('오펜하이머', 'Christopher Nolan', 'Thriller', 180, '/posters/오펜하이머.jpeg');
-        ('올빼미', '안태진', 'Thriller', 118, '/posters/올빼미.jpeg');
-        ('타겟', '박희곤', 'Thriller', 101, '/posters/타겟.jpeg');
-        ('끝까지 간다', '김성훈', 'Thriller', 111, '/posters/끝까지_간다.jpeg');
-        ('스즈메의 문단속', 'Makoto Niitsu', 'Animation', 122, '/posters/스즈메의_문단속.jpeg');
-        ('너의 이름은', 'Makoto Niitsu', 'Animation', 106, '/posters/너의_이름은.jpeg');
-        ('엘리멘탈', 'Peter Sohn', 'Animation', 109, '/posters/엘리멘탈.jpeg');
+        ('극한직업', '이병헌', 'Comedy', 111, '/posters/극한직업.jpeg'),
+        ('화사한 그녀', '이승준', 'Comedy', 121, '/posters/화사한_그녀.jpeg'),
+        ('30일', '남대중', 'Comedy', 119, '/posters/30일.jpeg'),
+        ('컨저링2', 'James Wan', 'Horror', 134, '/posters/컨저링2.jpeg'),
+        ('곡성', '나홍진', 'Horror', 156, '/posters/곡성.jpeg'),
+        ('엑스텐션', 'Alexandre Aja', 'Horror', 85, '/posters/엑스텐션.jpeg'),
+        ('라이트 아웃', 'David F. Sandberg', 'Horror', 81, '/posters/라이트_아웃.jpeg'),
+        ('미녀와 야수', 'Bill Condon', 'Romance', 129, '/posters/미녀와_야수.jpeg'),
+        ('노트북', 'Nick Cassavetes', 'Romance', 123, '/posters/노트북.jpeg'),
+        ('어바웃 타임', 'Richard Curtis', 'Romance', 123, '/posters/어바웃_타임.jpeg'),
+        ('500일의 썸머', 'Marc Webb', 'Romance', 95, '/posters/500일의_썸머.jpeg'),
+        ('크리에이터', 'Gareth Edwards', 'SF', 133, '/posters/크리에이터.jpeg'),
+        ('인터스텔라', 'Christopher Nolan', 'SF', 169, '/posters/인터스텔라.jpeg'),
+        ('레디 플레이어 원', 'Steven Spielberg', 'SF', 140, '/posters/레디_플레이어_원.jpeg'),
+        ('듄', 'Denis Villeneuve', 'SF', 155, '/posters/듄.jpeg'),
+        ('오펜하이머', 'Christopher Nolan', 'Thriller', 180, '/posters/오펜하이머.jpeg'),
+        ('올빼미', '안태진', 'Thriller', 118, '/posters/올빼미.jpeg'),
+        ('타겟', '박희곤', 'Thriller', 101, '/posters/타겟.jpeg'),
+        ('끝까지 간다', '김성훈', 'Thriller', 111, '/posters/끝까지_간다.jpeg'),
+        ('스즈메의 문단속', 'Makoto Niitsu', 'Animation', 122, '/posters/스즈메의_문단속.jpeg'),
+        ('너의 이름은', 'Makoto Niitsu', 'Animation', 106, '/posters/너의_이름은.jpeg'),
+        ('엘리멘탈', 'Peter Sohn', 'Animation', 109, '/posters/엘리멘탈.jpeg'),
         ('겨울왕국', 'Chris Buck', 'Animation', 108, '/posters/겨울왕국.jpeg');
     `;
 
@@ -104,38 +104,38 @@ const insertMoviesOttRelation = () => {
         (8, 1),
         (9, 2),
         (9, 4),
-        (10, 5),
-        (11, 2),
-        (11, 1),
-        (12, 2),
-        (13, 1),
-        (13, 2),
-        (13, 4),
-        (14, 3),
-        (15, 5),
-        (16, 1),
-        (16, 2),
-        (17, 4),
-        (18, 3),
-        (19, 1),
-        (19, 3),
-        (20, 1),
-        (21, 2),
-        (22, 3),
-        (22, 4),
-        (23, 5),
-        (24, 1),
-        (24, 5),
-        (25, 1),
-        (26, 2),
-        (27, 3),
-        (28, 4),
-        (29, 2),
-        (29, 1),
-        (30, 2),
-        (30, 4),
-        (31, 5),
-        (32, 2);
+        (42, 5),
+        (43, 2),
+        (43, 1),
+        (44, 2),
+        (45, 1),
+        (45, 2),
+        (45, 4),
+        (46, 3),
+        (47, 5),
+        (48, 1),
+        (48, 2),
+        (49, 4),
+        (50, 3),
+        (51, 1),
+        (51, 3),
+        (52, 1),
+        (53, 2),
+        (54, 3),
+        (54, 4),
+        (55, 5),
+        (56, 1),
+        (56, 5),
+        (57, 1),
+        (58, 2),
+        (59, 3),
+        (60, 4),
+        (61, 2),
+        (61, 1),
+        (62, 2),
+        (62, 4),
+        (63, 5),
+        (64, 2);
     `;
 
     connection.query(query, (err, results) => {
